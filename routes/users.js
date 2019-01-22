7855f41706fc756a92f59cfc2a4880d6aacb228a
rosen777 <rosen.toshev@flatironschool.com>
2019-01-22 09:44:35 +0000

    Model has been imported into sequelize

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models')
 
 /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send(
+// {users: [
+//     {id: 1, username: "Count Chocula"},
+//     {id: 2, username: "Munch Monster"},
+//   ]
+//
+//
+// })
+//
+// });
+
+
 router.get('/', function(req, res, next) {
-  res.send(
-{users: [
-    {id: 1, username: "Count Chocula"},
-    {id: 2, username: "Munch Monster"},
-  ]
-
-
+    models.User.findAll().then(function(users){
+      // console.log(users, fields)
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({users: users}))
+  })
 })
 
-});
 
+// router.post('/', function(req, res, next) => {
+//   models.User.findAll({where: {id: 1}}).then(function(user) {
+//     res.send(user)
+//   })
+// });
 
 
 router.post('/', (req, res) => {
